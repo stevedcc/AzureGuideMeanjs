9fbcf0b629c51784814ba97fa3d4bfad1277e21b
Steve Crawford <steve.crawford@sdx-ag.de>
2018-04-07 13:55:24 +0200

    added Application Insights support

--- server.js
+++ server.js
@@ -4,4 +4,7 @@
  * Module dependencies.
  */
 var app = require('./config/lib/app');
+const appInsights = require("applicationInsights");
+appInsights.setup("4de2599d-e324-4340-a639-b29baa785074");
+appInsights.start();
 var server = app.start();
